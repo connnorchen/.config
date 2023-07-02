eb64603a8e9464158b9531550807a2b025910339
Shengxian Chen <connor@Shengxians-Air.attlocal.net>
2023-07-02 15:39:01 -0700

    added tmux config and nvim config

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,46 @@
+vim.keymap.set('i', '<C-c>', '<Esc>')
+
+-- visual mode J, K make visual block move around
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- pressing J will make cursor in place
+vim.keymap.set("n", "J", "mzJ`z")
+-- Ctrl + d, Ctrl + u moves the screen in an oriented way
+vim.keymap.set("n", "<C-d>", "<C-d>zz") vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- search terms are kept in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- replace won't erase paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+
+vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- remap split retrival
+vim.keymap.set("n", "<leader>k", "<C-w><up>")
+vim.keymap.set("n", "<leader>j", "<C-w><down>")
+vim.keymap.set("n", "<leader>h", "<C-w><left>")
+vim.keymap.set("n", "<leader>l", "<C-w><right>")
+
+-- remap tab movement
+vim.keymap.set("n", "<leader>tl", ":tabn<CR>")
+vim.keymap.set("n", "<leader>th", ":tabp<CR>")
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
+
+-- remap split movement
+vim.keymap.set("n", "<leader>ss", "<C-w>s")
+vim.keymap.set("n", "<leader>sv", "<C-w>v")
+
+-- remap tmux session command
+vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,60 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'ellisonleao/gruvbox.nvim'
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use'nvim-treesitter/playground'
+  use 'VonHeikemen/lsp-zero.nvim'
+  use {
+    -- LSP support
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+
+    -- auto-complete
+    'hrsh7th/nvim-cmp',     -- Required
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lsp', -- Required
+    'hrsh7th/cmp-nvim-lua',
+
+    -- Snippets
+    'L3MON4D3/LuaSnip',     -- Required
+    'rafamadriz/friendly-snippets'
+  }
+  use 'theprimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+  use {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
