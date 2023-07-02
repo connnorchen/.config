1a6b6effc8d128c50d9a5ab96166b22dfa46756c
Shengxian Chen <connor@Shengxians-Air.attlocal.net>
2023-07-02 16:33:20 -0700

    add swapping command between windows

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -44,3 +44,7 @@ vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
 vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
 vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+
+-- remap swap windows command
+vim.keymap.set("n", "<leader>w", "<C-w>w")
+
